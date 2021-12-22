5bfff5dbc6c00aad0c41509fa8cee25c73031b43
Guillaume Merle <guillaume.mrle@gmail.com>
2021-12-22 17:35:11 +0100

    Add extended column and row widgets to support spacing between items

---
+++ lib/components/extended_widgets.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SpacedColumn extends Column {
+  SpacedColumn({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    List<Widget> children = const <Widget>[],
+    double spacing = 0,
+  }) : super(
+          // children: addSpacers(children, SizedBox(height: spacing)),
+          children: [
+            for (var e in children) ...[e, SizedBox(height: spacing)]
+          ],
+          key: key,
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          crossAxisAlignment: crossAxisAlignment,
+          textDirection: textDirection,
+          verticalDirection: verticalDirection,
+          textBaseline: textBaseline,
+        );
+}
+
+class SpacedRow extends Row {
+  SpacedRow({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    List<Widget> children = const <Widget>[],
+    double spacing = 0,
+  }) : super(
+          children: [
+            for (var e in children) ...[e, SizedBox(width: spacing)]
+          ],
+          key: key,
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          crossAxisAlignment: crossAxisAlignment,
+          textDirection: textDirection,
+          verticalDirection: verticalDirection,
+          textBaseline: textBaseline,
+        );
+}
