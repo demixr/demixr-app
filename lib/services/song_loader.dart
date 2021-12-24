eea54579d2e77614bfc8f970fb84cddf40088118
Guillaume Merle <guillaume.mrle@gmail.com>
2021-12-24 10:17:02 +0100

    SongLoader service with ability to load from device using the file picker

---
+++ lib/services/song_loader.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:demixr_app/models/failure/failure.dart';
+import 'package:demixr_app/models/failure/no_song_picked.dart';
+import 'package:demixr_app/models/song.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
+
+class SongLoader {
+  Future<Either<Failure, Song>> getFromDevice() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.audio);
+
+    if (result == null) return Left(NoSongPickedFailure());
+
+    PlatformFile file = result.files.single;
+    File path = File(file.path!);
+    var metadata = await MetadataRetriever.fromFile(path);
+
+    return Right(
+      Song(
+        title: metadata.trackName,
+        artists: metadata.trackArtistNames,
+        file: path,
+        cover: metadata.albumArt,
+      ),
+    );
+  }
+}
