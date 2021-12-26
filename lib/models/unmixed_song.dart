c965c576ab01b665080c75d8b44cfe44356d60ec
Guillaume Merle <guillaume.mrle@gmail.com>
2021-12-26 16:41:49 +0100

    Add UnmixedSong model with Hive annotations

---
+++ lib/models/unmixed_song.dart
@@ -0,0 +1,32 @@
+// part 'unmixed_song.g.dart';
+import 'package:hive/hive.dart';
+
+import 'song.dart';
+
+part 'unmixed_song.g.dart';
+
+@HiveType(typeId: 0)
+class UnmixedSong {
+  @HiveField(0)
+  Song mixture;
+
+  @HiveField(1)
+  Song vocals;
+
+  @HiveField(2)
+  Song bass;
+
+  @HiveField(3)
+  Song drums;
+
+  @HiveField(4)
+  Song other;
+
+  UnmixedSong({
+    required this.mixture,
+    required this.vocals,
+    required this.bass,
+    required this.drums,
+    required this.other,
+  });
+}
