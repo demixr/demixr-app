b399a94a352b81dfd4bfc7ecba9033f2ee658839
Guillaume Merle <guillaume.mrle@gmail.com>
2022-01-01 16:45:04 +0100

    Update song storage in library repository

--- lib/repositories/library_repository.dart
+++ lib/repositories/library_repository.dart
@@ -5,12 +5,14 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
-import '../utils.dart' show moveFile;
+import '../utils.dart';
 import '../constants.dart' show BoxesNames;
 
 class LibraryRepository {
   final _box = Hive.box<UnmixedSong>(BoxesNames.library);
   final _directoryName = 'library';
+
+  Box<UnmixedSong> get box => _box;
 
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
@@ -20,27 +22,26 @@ class LibraryRepository {
   Future<String> get _directoryPath async {
     final path = await _localPath;
     var directory = Directory(p.join(path, _directoryName));
+    directory = await directory.createIfNotPresent();
+    return directory.path;
+  }
 
-    if (await directory.exists()) {
-      return directory.path;
-    } else {
-      directory = await directory.create(recursive: true);
-      return directory.path;
-    }
+  Future<String> _createSongDirectory(
+      String libraryDirectory, Song song) async {
+    var directory = Directory(p.join(libraryDirectory, song.toString()));
+    directory = await directory.createUnique();
+    return directory.path;
   }
 
   Future<Song> saveFile(Song song) async {
     String libraryDirectory = await _directoryPath;
-    String filename = "${song}_mixture${p.extension(song.path)}";
-    String songPath = p.join(libraryDirectory, filename);
+    String songDirectory = await _createSongDirectory(libraryDirectory, song);
+    String filename = "mixture${p.extension(song.path)}";
+    String songPath = p.join(songDirectory, filename);
 
     final savedFile = await moveFile(File(song.path), songPath);
     song.path = savedFile.path;
 
     return song;
-  }
-
-  void add(UnmixedSong song) {
-    _box.add(song);
   }
 }
