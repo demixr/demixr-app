70f3b3feb6b8ba64343f1555123d658441214502
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2022-01-06 19:53:54 +0100

    Read wav file in by chunks of 500k frames

--- android/app/src/main/java/com/demixr/demixr_app/MainActivity.java
+++ android/app/src/main/java/com/demixr/demixr_app/MainActivity.java
@@ -16,87 +16,91 @@ import java.util.Arrays;
 public class MainActivity extends FlutterActivity {
   private static Module module;
 
-  void recognize(String audioPath, String modelPath, String outputDir) throws IOException, WavFileException {
-    if (module == null) {
-      module = LiteModuleLoader.load(audioPath);
-    }
-
-    File toSeparate = new File(modelPath);
-
-    // Open the wav file specified as the first argument
-    WavFile wavFile = WavFile.openWavFile(toSeparate);
-
-    // Get the number of audio channels in the wav file
-    int numChannels = wavFile.getNumChannels();
-    int numFrames = (int) wavFile.getNumFrames();
-
-    // Create a buffer
-    double[] buffer = new double[numFrames * numChannels];
-
-    int framesRead;
-
-    do
-    {
-      // Read frames into buffer
-      framesRead = wavFile.readFrames(buffer, numFrames);
-    }
-    while (framesRead != 0);
-
-    // Close the wavFile
-    wavFile.close();
-
-    double[][] audio = new double[2][(int) wavFile.getNumFrames()];
-
-    for (int i = 0; i < wavFile.getNumFrames(); i++) {
-      audio[0][i] = buffer[i * 2];
-      audio[1][i] = buffer[i * 2 + 1];
-    }
-
-    double[] flatArray = Arrays.stream(audio)
-            .flatMapToDouble(Arrays::stream)
-            .toArray();
-
-    float[] floatArray = new float[flatArray.length];
-    for (int i = 0 ; i < flatArray.length; i++)
-    {
-      floatArray[i] = (float) flatArray[i];
-    }
-
-    Tensor inTensor = Tensor.fromBlob(floatArray, new long[]{1, 2, numFrames});
-    IValue result = module.forward(IValue.from(inTensor));
-    Tensor newTensor = result.toTensor();
-    float[] newWav = newTensor.getDataAsFloatArray();
-
-    int numStems = 4;
-    double[][][] reshaped = new double[numStems][numChannels][numFrames];
-
-    for (int i = 0; i < numStems; i++) {
-      for (int j = 0; j < numChannels; j++) {
-        for (int k = 0; k < numFrames; k++) {
-          reshaped[i][j][k] = newWav[i * numFrames * numChannels + j * numFrames + k];
+  void separate(String audioPath, String modelPath, String outputDir) throws IOException, WavFileException {
+      if (module == null) {
+            module = LiteModuleLoader.load(modelPath);
         }
-      }
-    }
 
-    // create files for separated output
-    try
-    {
-      int sampleRate = 44100;    // Samples per second
+        File toSeparate = new File(audioPath);
 
-      for (int i = 0; i < numStems; i++) {
-        File out = new File(outputDir, "stem_" + i + ".wav");
-        out.createNewFile();
-        WavFile sepFile = WavFile.newWavFile(out, 2, numFrames, 16, sampleRate);
+        // Open the wav file specified as the first argument
+        WavFile wavFile = WavFile.openWavFile(toSeparate);
 
-        sepFile.writeFrames(reshaped[i], numFrames);
+        // Get the number of audio channels in the wav file
+        int numChannels = wavFile.getNumChannels();
+        int numFrames = (int) wavFile.getNumFrames();
+        int numStems = 4;
+
+        // Create a buffer of 500 000 frames
+        int nbBufferFrame = 500000;
+        double[] buffer = new double[nbBufferFrame * numChannels];
+
+        // Read frames into buffer
+        int framesRead = wavFile.readFrames(buffer, nbBufferFrame);
+
+        double[][][] finalStems = new double[numStems][numChannels][numFrames];
+        int chunkCount = 0;
+
+        while (framesRead != 0) {
+            double[][] audio = new double[2][framesRead];
+
+            for (int i = 0; i < framesRead; i++) {
+                audio[0][i] = buffer[i * 2];
+                audio[1][i] = buffer[i * 2 + 1];
+            }
+
+            // Flatten array
+            double[] doubleFlatArray = Arrays.stream(audio)
+                    .flatMapToDouble(Arrays::stream)
+                    .toArray();
+
+            // Convert double array to float array
+            float[] floatFlatArray = new float[doubleFlatArray.length];
+            for (int i = 0 ; i < doubleFlatArray.length; i++) {
+                floatFlatArray[i] = (float) doubleFlatArray[i];
+            }
+
+            // Create Tensor from flattened array
+            Tensor inTensor = Tensor.fromBlob(floatFlatArray, new long[]{1, 2, framesRead});
+
+            // Model inference
+            IValue result = module.forward(IValue.from(inTensor));
+            Tensor resultTensor = result.toTensor();
+            float[] resultStems = resultTensor.getDataAsFloatArray();
+
+            for (int i = 0; i < numStems; i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    for (int k = 0; k < framesRead; k++) {
+                        finalStems[i][j][k + chunkCount * nbBufferFrame] = resultStems[i * framesRead * numChannels + j * framesRead + k];
+                    }
+                }
+            }
+            framesRead = wavFile.readFrames(buffer, nbBufferFrame);
+            chunkCount++;
+        }
 
         // Close the wavFile
-        sepFile.close();
-      }
+        wavFile.close();
+
+        // create files for separated output
+        try
+        {
+            int sampleRate = 44100;    // Samples per second
+
+            for (int i = 0; i < numStems; i++) {
+                File out = new File(outputDir, "stem_" + i + ".wav");
+                out.createNewFile();
+                WavFile sepFile = WavFile.newWavFile(out, 2, numFrames, 16, sampleRate);
+
+                sepFile.writeFrames(finalStems[i], numFrames);
+
+                // Close the wavFile
+                sepFile.close();
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
     }
-    catch (Exception e)
-    {
-      System.err.println(e);
-    }
-  }
 }
