3d589ada4b34938a562d09dc102644c166de8982
Alexandre DEISS <alexandre.deiss@epita.fr>
2022-01-19 16:16:56 +0100

    Use youtube API package to load trending package

--- lib/providers/youtube_provider.dart
+++ lib/providers/youtube_provider.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:youtube_api/youtube_api.dart';
 
 class YoutubeProvider extends ChangeNotifier {
 
   final String _apiKey = dotenv.env['YOUTUBE_API_KEY'] ?? '';
+  late YoutubeAPI api;
+  List<YouTubeVideo> _videos = [];
+  final String _regionCode = 'US';
+
+  YoutubeProvider() {
+    api = YoutubeAPI(_apiKey);
+    loadTrends();
+  }
+
+  Future<void> loadTrends() async {
+    _videos = await api.getTrends(regionCode: _regionCode);
+    notifyListeners();
+  }
 }
