4ee7d72e2f640285bba0ff2805367a6745a1579d
Guillaume Merle <guillaume.mrle@gmail.com>
2022-01-29 11:41:25 +0100

    LibraryProvider documentation

--- lib/providers/library_provider.dart
+++ lib/providers/library_provider.dart
@@ -1,10 +1,14 @@
-import 'package:dartz/dartz.dart';
-import 'package:demixr_app/models/failure/failure.dart';
-import 'package:demixr_app/models/failure/no_song_selected.dart';
-import 'package:demixr_app/models/unmixed_song.dart';
-import 'package:demixr_app/repositories/library_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:dartz/dartz.dart';
 
+import '../models/unmixed_song.dart';
+import '../models/failure/failure.dart';
+import '../models/failure/no_song_selected.dart';
+import '../repositories/library_repository.dart';
+
+/// Provider for the unmixed songs library.
+///
+/// Uses the [LibraryRepository] to store and get the songs.
 class LibraryProvider extends ChangeNotifier {
   final _repository = LibraryRepository();
   List<UnmixedSong> _songs = [];
@@ -14,17 +18,22 @@ class LibraryProvider extends ChangeNotifier {
     _loadSongs();
   }
 
+  /// The number of songs in the library.
   int get numberOfSongs => _songs.length;
 
+  /// The state of the library, empty or not.
   bool get isEmpty => _songs.isEmpty;
 
+  /// List of the songs in the library.
   List<UnmixedSong> get songList => _songs;
 
+  /// The current selected song, from it's index.
   Either<Failure, UnmixedSong> get currentSong => _currentSongIndex.fold(
         (noSongSelected) => Left(noSongSelected),
         (index) => Right(getAt(index)),
       );
 
+  /// Sets the current song [index].
   bool setCurrentSongIndex(int index) {
     if (index >= numberOfSongs || index < 0) return false;
 
@@ -34,17 +43,24 @@ class LibraryProvider extends ChangeNotifier {
     return true;
   }
 
+  /// Returns an index based on the chosen display order, here reverse.
   int getIndexByOrder(int index) => numberOfSongs - index - 1;
 
+  /// Returns true if the given [index] matches the currently selected song.
   bool matchSelectedSong(int index) => _currentSongIndex.fold(
       (noSongSelected) => false, (songIndex) => index == songIndex);
 
+  /// Returns the [UnmixedSong] at the given [index].
   UnmixedSong getAt(int index) {
     return _songs.elementAt(index);
   }
 
+  /// Loads the songs stored in the [_repository] in [_songs].
   void _loadSongs() => _songs = _repository.box.values.toList();
 
+  /// Saves the given [song] in the [_repository].
+  ///
+  /// Returns the index of the saved song.
   Future<int> saveSong(UnmixedSong song) async {
     song = await _repository.saveFiles(song);
     _repository.box.add(song);
@@ -53,6 +69,7 @@ class LibraryProvider extends ChangeNotifier {
     return numberOfSongs - 1;
   }
 
+  /// Removes the song at the given [index] from the library.
   void removeSong(int index) {
     // if the song to remove is the selected song, unselect it
     _currentSongIndex.fold(
@@ -70,6 +87,7 @@ class LibraryProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Selects the next song in the library.
   bool nextSong() {
     return _currentSongIndex.fold(
       (noSongSelected) => false,
@@ -77,6 +95,7 @@ class LibraryProvider extends ChangeNotifier {
     );
   }
 
+  /// Selects the previous in the library.
   bool previousSong() {
     return _currentSongIndex.fold(
       (noSongSelected) => false,
