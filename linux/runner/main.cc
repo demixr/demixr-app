5efaa069bd6ffab8b2136660c3698d5a7ac710d8
Marc Demoustier <marc.demoustier@epita.fr>
2026-07-14 14:41:45 +0800

    Name the Linux runner after Demixr

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -1,6 +1,6 @@
-#include "my_application.h"
+#include "demixr_application.h"
 
 int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
+  g_autoptr(DemixrApplication) app = demixr_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
